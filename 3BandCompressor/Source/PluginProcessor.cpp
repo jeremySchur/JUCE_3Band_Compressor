a9181e32602a341d74492ca32052deb92d9507b3
Jeremy Schur <jeremy.schur@telosalliance.com>
2025-05-24 16:06:51 -0600

    Add LinkwitzRiley filtering

--- 3BandCompressor/Source/PluginProcessor.cpp
+++ 3BandCompressor/Source/PluginProcessor.cpp
@@ -49,6 +49,11 @@ _3BandCompressorAudioProcessor::_3BandCompressorAudioProcessor()
     floatHelper(compressor.threshold, Names::Threshold_Low_Band);
     choiceHelper(compressor.ratio, Names::Ratio_Low_Band);
     boolHelper(compressor.bypassed, Names::Bypassed_Low_Band);
+
+    floatHelper(lowCrossover, Names::Low_Mid_Crossover_Freq);
+
+    LP.setType(juce::dsp::LinkwitzRileyFilterType::lowpass);
+    HP.setType(juce::dsp::LinkwitzRileyFilterType::highpass);
 }
 
 _3BandCompressorAudioProcessor::~_3BandCompressorAudioProcessor()
@@ -128,6 +133,13 @@ void _3BandCompressorAudioProcessor::prepareToPlay (double sampleRate, int sampl
     spec.sampleRate = sampleRate;
 
     compressor.prepare(spec);
+
+    LP.prepare(spec);
+    HP.prepare(spec);
+
+    for (auto& buffer : filterBuffers) {
+        buffer.setSize(spec.numChannels, samplesPerBlock);
+    }
 }
 
 void _3BandCompressorAudioProcessor::releaseResources()
@@ -177,8 +189,40 @@ void _3BandCompressorAudioProcessor::processBlock (juce::AudioBuffer<float>& buf
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    compressor.updateCompressorSettings();
-    compressor.process(buffer);
+    /*compressor.updateCompressorSettings();
+    compressor.process(buffer);*/
+
+    for (auto& fb : filterBuffers) {
+        fb = buffer;
+    }
+
+    auto cutoff = lowCrossover->get();
+    LP.setCutoffFrequency(cutoff);
+    HP.setCutoffFrequency(cutoff);
+
+    auto fb0Block = juce::dsp::AudioBlock<float>(filterBuffers[0]);
+    auto fb1Block = juce::dsp::AudioBlock<float>(filterBuffers[1]);
+
+    auto fb0Ctx = juce::dsp::ProcessContextReplacing<float>(fb0Block);
+    auto fb1Ctx = juce::dsp::ProcessContextReplacing<float>(fb1Block);
+
+    LP.process(fb0Ctx);
+    HP.process(fb1Ctx);
+
+    auto numSamples = buffer.getNumSamples();
+    auto numChannels = buffer.getNumChannels();
+
+    buffer.clear();
+
+    auto addFilterBand = [ns = numSamples, nc = numChannels](auto& inputBuffer, const auto& source)
+    {
+        for (auto i = 0; i < nc; ++i) {
+            inputBuffer.addFrom(i, 0, source, i, 0, ns);
+        }
+    };
+
+    addFilterBand(buffer, filterBuffers[0]);
+    addFilterBand(buffer, filterBuffers[1]);
 }
 
 //==============================================================================
@@ -257,6 +301,12 @@ juce::AudioProcessorValueTreeState::ParameterLayout _3BandCompressorAudioProcess
     layout.add(std::make_unique<AudioParameterBool>(
         params.at(Names::Bypassed_Low_Band), params.at(Names::Bypassed_Low_Band),
         false
+    ));
+
+    layout.add(std::make_unique<AudioParameterFloat>(
+        params.at(Names::Low_Mid_Crossover_Freq), params.at(Names::Low_Mid_Crossover_Freq),
+        NormalisableRange<float>(20, 20000, 1, 1),
+        500
     ));
 
     return layout;
